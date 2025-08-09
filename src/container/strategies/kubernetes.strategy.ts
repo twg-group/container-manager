601d3aa160d3993d5ede313c0aecee67e057735f
Matvey Alekseev <mat@twg.su>
2025-08-10 00:41:23 +0300

    updates

--- src/container/strategies/kubernetes.strategy.ts
+++ src/container/strategies/kubernetes.strategy.ts
@@ -6,21 +6,26 @@ import { LogDto } from '@dto/log.dto';
 
 @Injectable()
 export class KubernetesStrategy extends BaseStrategy {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   start(id: string): Promise<void> {
     throw new Error('Method not implemented.');
   }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   stop(id: string, timeout?: number): Promise<void> {
     throw new Error('Method not implemented.');
   }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   deploy(config: DeployConfigDto): Promise<string> {
     throw new Error('Method not implemented.');
   }
   list(): Promise<InfoDto[]> {
     throw new Error('Method not implemented.');
   }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   remove(id: string): Promise<void> {
     throw new Error('Method not implemented.');
   }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   logs(id: string, since?: string, tail?: number): Promise<LogDto[]> {
     throw new Error('Method not implemented.');
   }
